98c8e9c203d3b8d37158a5ee11a3ac429ee0b944
akihiko47 <akihiko4701a@gmail.com>
2025-02-05 23:38:44 +0300

    Swapchain creation

--- src/vk-engine.cpp
+++ src/vk-engine.cpp
@@ -3,11 +3,16 @@
 #include <SDL.h>
 #include <SDL_vulkan.h>
 
+#include <VkBootstrap.h>
+
 #include <vk-initializers.hpp>
 #include <vk-types.hpp>
 
 #include <chrono>
 #include <thread>
+
+
+const bool USE_VALIDATION_LAYERS = true;
 
 
 using namespace vr;
@@ -25,6 +30,34 @@ VulkanEngine::~VulkanEngine() {
 void VulkanEngine::Init() {
 	std::cout << "Vulkan engine init\n";
 
+	CreateWindow();
+	InitVulkan();
+	InitSwapchain();
+	InitCommands();
+	InitSyncStructures();
+
+	m_isInitialized = true;
+}
+
+
+void VulkanEngine::Cleanup() {
+	std::cout << "Vulkan engine cleanup\n";
+
+	if (m_isInitialized) {
+		DestroySwapChain();
+
+		vkDestroySurfaceKHR(m_instance, m_surface, nullptr);
+		vkDestroyDevice(m_device, nullptr);
+
+		vkb::destroy_debug_utils_messenger(m_instance, m_debugMessenger);
+		vkDestroyInstance(m_instance, nullptr);
+		SDL_DestroyWindow(m_window);
+	}
+}
+
+
+void VulkanEngine::CreateWindow() {
+
 	// we want to use window and input systems from SDL
 	SDL_Init(SDL_INIT_VIDEO);
 
@@ -38,17 +71,103 @@ void VulkanEngine::Init() {
 		m_windowExtent.height,
 		windowFlags
 	);
-
-	m_isInitialized = true;
 }
 
 
-void VulkanEngine::Cleanup() {
-	std::cout << "Vulkan engine cleanup\n";
+void VulkanEngine::InitVulkan() {
 
-	if (m_isInitialized) {
-		SDL_DestroyWindow(m_window);
+	// instance creation
+	vkb::InstanceBuilder instanceBuilder;
+
+	vkb::Result instanceRes = instanceBuilder.set_app_name("VulkanRenderer")
+		.request_validation_layers(USE_VALIDATION_LAYERS)
+		.use_default_debug_messenger()
+		.require_api_version(1, 3, 0)
+		.build();
+	vkb::Instance vkbInstance= instanceRes.value();
+
+	m_instance = vkbInstance.instance;
+	m_debugMessenger = vkbInstance.debug_messenger;
+
+	// surface creation
+	SDL_Vulkan_CreateSurface(m_window, m_instance, &m_surface);
+
+	// physical device creation
+	VkPhysicalDeviceVulkan13Features features13{};
+	features13.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_3_FEATURES;
+	features13.dynamicRendering = true;
+	features13.synchronization2 = true;
+
+	VkPhysicalDeviceVulkan12Features features12{};
+	features12.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_2_FEATURES;
+	features12.bufferDeviceAddress = true;
+	features12.descriptorIndexing = true;
+
+	vkb::PhysicalDeviceSelector selector{vkbInstance};
+	vkb::PhysicalDevice vkbPhysicalDevice = selector
+		.set_minimum_version(1, 3)
+		.set_required_features_13(features13)
+		.set_required_features_12(features12)
+		.set_surface(m_surface)
+		.select()
+		.value();
+
+	m_physicalDevice = vkbPhysicalDevice.physical_device;
+
+	// device creation
+	vkb::DeviceBuilder deviceBuilder{vkbPhysicalDevice};
+	vkb::Device vkbDevice = deviceBuilder.build().value();
+
+	m_device = vkbDevice.device;
+}
+
+
+void VulkanEngine::InitSwapchain() {
+	CreateSwapChain(m_windowExtent.width, m_windowExtent.height);
+}
+
+
+void VulkanEngine::CreateSwapChain(uint32_t width, uint32_t height) {
+	vkb::SwapchainBuilder swapChainBuilder{m_physicalDevice, m_device, m_surface};
+
+	m_swapChainImageFormat = VK_FORMAT_B8G8R8A8_UNORM;
+	VkSurfaceFormatKHR surfaceFormat{};
+	surfaceFormat.format = m_swapChainImageFormat;
+	surfaceFormat.colorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR;
+
+	vkb::Swapchain vkbSwapchain = swapChainBuilder
+		.set_desired_format(surfaceFormat)
+		.set_desired_present_mode(VK_PRESENT_MODE_FIFO_KHR)
+		.set_desired_extent(width, height)
+		.add_image_usage_flags(VK_IMAGE_USAGE_TRANSFER_DST_BIT)
+		.build()
+		.value();
+
+	m_swapChainExtent = vkbSwapchain.extent;
+	m_swapChain = vkbSwapchain.swapchain;
+	m_swapChainImages = vkbSwapchain.get_images().value();
+	m_swapChainImageViews = vkbSwapchain.get_image_views().value();
+}
+
+
+void VulkanEngine::DestroySwapChain() {
+	vkDestroySwapchainKHR(m_device, m_swapChain, nullptr);
+
+	// deletes images as well
+	for (auto swapChainImageView : m_swapChainImageViews) {
+		vkDestroyImageView(m_device, swapChainImageView, nullptr);
 	}
+}
+
+
+
+void VulkanEngine::InitCommands() {
+
+}
+
+
+void VulkanEngine::InitSyncStructures() {
+
 }
 
 
--- src/vk-engine.hpp
+++ src/vk-engine.hpp
@@ -16,12 +16,38 @@ namespace vr {
 		void Draw();
 		void Cleanup();
 
+		// sdl window creation
+		void CreateWindow();
+
+		// vulkan initialization
+		void InitVulkan();
+		void InitSwapchain();
+		void CreateSwapChain(uint32_t width, uint32_t height);
+		void DestroySwapChain();
+		void InitCommands();
+		void InitSyncStructures();
+
 	private:
 		bool       m_isInitialized;
 		int        m_frameNumber;
 		bool       m_stopRendering;
 		VkExtent2D m_windowExtent;
 
+		// sdl window stuff
 		SDL_Window *m_window;
+
+		// vulkan stuff
+		VkInstance m_instance;
+		VkDebugUtilsMessengerEXT m_debugMessenger;
+		VkPhysicalDevice m_physicalDevice;
+		VkDevice m_device;
+		VkSurfaceKHR m_surface;
+
+		// swapchain stuff
+		VkSwapchainKHR m_swapChain;
+		VkFormat m_swapChainImageFormat;
+		std::vector<VkImage> m_swapChainImages;
+		std::vector<VkImageView> m_swapChainImageViews;
+		VkExtent2D m_swapChainExtent;
 	};
 }
