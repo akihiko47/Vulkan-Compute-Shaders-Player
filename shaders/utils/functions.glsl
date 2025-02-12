b2462914b49e2f6ce2fe9f1e438da341a0c422e8
akihiko47 <akihiko4701a@gmail.com>
2025-02-12 18:33:53 +0300

    Radar shader

--- shaders/utils/functions.glsl
+++ shaders/utils/functions.glsl
@@ -1,12 +1,6 @@
 #define TWO_PI 6.28318530718
 #define PI     3.14159265359
 
-
-float atan2(float y, float x) {
-    bool s = (abs(x) > abs(y));
-    return mix(PI/2.0 - atan(x,y), atan(y,x), s);
-}
-
 vec2 rotate(vec2 uv, float th){
     return mat2(cos(th), sin(th), -sin(th), cos(th)) * uv;
 }
